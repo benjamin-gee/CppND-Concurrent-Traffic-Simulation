b696da6a038a8b3e50802a602c8a71f6dc4a5aa0
Benjamin Gee <benjamin.n.gee@gmail.com>
2020-07-21 05:03:55 +0000

    FP1 - Declared class TrafficLight in header file to match definitions in cpp file

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -8,6 +8,7 @@
 
 // forward declarations to avoid include cycle
 class Vehicle;
+enum TrafficLightPhase {red, green};
 
 
 // FP.3 Define a class „MessageQueue“ which has the public methods send and receive. 
@@ -30,17 +31,24 @@ private:
 // can be either „red“ or „green“. Also, add the private method „void cycleThroughPhases()“. 
 // Furthermore, there shall be the private member _currentPhase which can take „red“ or „green“ as its value. 
 
-class TrafficLight
+class TrafficLight : public TrafficObject
 {
 public:
     // constructor / desctructor
+    TrafficLight();
+    ~TrafficLight();
 
     // getters / setters
-
+    TrafficLightPhase getCurrentPhase();
+    
     // typical behaviour methods
+    void waitForGreen();
+    void simulate();
+
 
 private:
     // typical behaviour methods
+    void cycleThroughPhases();
 
     // FP.4b : create a private member of type MessageQueue for messages of type TrafficLightPhase 
     // and use it within the infinite loop to push each new TrafficLightPhase into it by calling 
@@ -48,6 +56,7 @@ private:
 
     std::condition_variable _condition;
     std::mutex _mutex;
+    TrafficLightPhase _currentPhase;
 };
 
 #endif
